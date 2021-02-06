6effbcddfd82809f00303f806512d3b71000abd5
JJ <jj.vega86@gmail.com>
2021-02-06 13:54:56 -0500

    Added Table, TableHeader, TableBody, and Form components along with multiple methods in App.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Table from './components/Table/table.jsx';
+import Form from './components/Table/form.jsx';
 
-class App extends React.Component {
-    render(){
+class App extends Component {
+    state = {
+        characters: [] 
+    }
+
+    removeCharacter = (index) => {
+        const {characters} = this.state
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i!== index
+            }),
+        })
+    }
+
+    handleSubmit = (character) =>{
+        this.setState({characters: [...this.state.characters, character]})
+    }
+
+    render(){ // class component must contain render(){} and return only one parent element 
+        const {characters} = this.state
+
         return(
-            <div className="App">
-                <h1>Hello, World!</h1>
+            <div className="container">
+                <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+                <Form handleSubmit={this.handleSubmit}/>
             </div>
         )
     }
